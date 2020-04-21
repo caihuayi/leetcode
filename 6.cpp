b0f097592d218af615a4d6b40f57586cd43d26da
caihuayi <caihuayi159@126.com>
2020-04-21 20:21:19 +0800

    6.cpp modify

--- 6.cpp
+++ 6.cpp
@@ -9,14 +9,7 @@ using namespace std;
 class Solution {
 public:
     string convert(string s, int numRows) {
-        int i = 0;
-        int q = 0;
-        int s = 0;
-        int queue_count = 2*numRows-2;
-        for (i = 0; i < s.length(); i++)
-        {
-            
-        }
+        
     }
 };
 
